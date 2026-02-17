fb6e1a138b1db7b564e94156ac362abb4cf1b027
Maneren <maneren731@gmail.com>
2026-02-17 15:57:09 +0100

    further extract UploadBar component

--- app/src/components/mod.rs
+++ app/src/components/mod.rs
@@ -4,6 +4,7 @@ mod folder_download;
 mod loading;
 mod new_folder;
 mod upload;
+mod upload_bar;
 
 pub use breadcrumbs::Breadcrumbs;
 pub use file_entries::FileEntries;
@@ -11,3 +12,4 @@ pub use folder_download::FolderDownloads;
 pub use loading::Loading;
 pub use new_folder::NewFolderButton;
 pub use upload::FileUpload;
+pub use upload_bar::UploadBar;
