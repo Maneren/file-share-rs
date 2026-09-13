4258f86ffd1d58b54b964075ae7cdc9af6cf4c59
Maneren <maneren731@gmail.com>
2026-09-14 01:31:00 +0200

    refactor curl command construction

--- app/src/components/folder_download.rs
+++ app/src/components/folder_download.rs
@@ -10,7 +10,6 @@ fn method_url_query(path: &String, method: &str) -> String {
 
 #[component]
 pub fn FolderDownloads(path: Signal<PathBuf>) -> impl IntoView {
-    let base_name = path.with(|path| display_os_string(path.file_name().unwrap_or_default()));
     let base_path = Arc::new(format!(
         "/archive/{}",
         path.with(|path| display_os_string(path))
@@ -33,29 +32,32 @@ pub fn FolderDownloads(path: Signal<PathBuf>) -> impl IntoView {
 
     let fast_lan_list = move || {
         let curl_list_path = &*curl_list_path;
-        Method::ALL.iter().filter_map(|method| {
-            let label = method.as_str();
-            let curl_url = method_url_query(curl_list_path, label);
-            let flags = method.tar_extract_flags()?;
-            let view = view! {
-              <li>
-                <button
-                  class="px-3 min-w-20 text-left"
-                  title=format!(
-                    "Copies: curl -L \"<this server>{curl_url}\" | tar {flags} -vC ./{base_name}",
-                  )
-                  data-url=curl_url.clone()
-                  onclick=format!(
-                    "navigator.clipboard.writeText(`curl -L \"${{window.location.origin}}{curl_url}\" | tar {flags} -vC ./{base_name}`)",
-                  )
-                >
-                  Copy curl |
-                  {label}
-                </button>
-              </li>
-            };
-            Some(view)
-        }).collect::<Vec<_>>()
+        Method::ALL
+            .iter()
+            .filter_map(|method| {
+                let label = method.as_str();
+                let curl_url = method_url_query(curl_list_path, label);
+                let flags = method.tar_extract_flags()?;
+                let make_command = |host| format!("curl -L \'{host}{curl_url}\' | tar x {flags}");
+                let view = view! {
+                  <li>
+                    <button
+                      class="px-3 min-w-20 text-left"
+                      title=format!("Copies: {command}", command = make_command("<host>").trim())
+                      data-url=curl_url.clone()
+                      onclick=format!(
+                        "navigator.clipboard.writeText(`{command}`)",
+                        command = make_command("${window.location.origin}").trim(),
+                      )
+                    >
+                      Copy curl |
+                      {label}
+                    </button>
+                  </li>
+                };
+                Some(view)
+            })
+            .collect::<Vec<_>>()
     };
 
     view! {
@@ -64,8 +66,9 @@ pub fn FolderDownloads(path: Signal<PathBuf>) -> impl IntoView {
           Download Folder
         </label>
         <ul tabindex="0" class="p-2 shadow dropdown-content menu bg-base-100 rounded-box">
+          <li class="menu-title">Archive</li>
           {method_list}
-          <li class="menu-title">Fast LAN</li>
+          <li class="menu-title">Curl-stream</li>
           {fast_lan_list}
         </ul>
       </div>
