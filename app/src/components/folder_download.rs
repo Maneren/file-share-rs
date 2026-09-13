a61f6bb0ca1a967c600d08ce48cf0cc39ade5960
Maneren <maneren731@gmail.com>
2026-09-13 11:24:30 +0200

    add copy curl-pipe-tar command to folder download menu

--- app/src/components/folder_download.rs
+++ app/src/components/folder_download.rs
@@ -1,19 +1,53 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::Arc};
 
 use leptos::prelude::*;
 
 use crate::utils::display_os_string;
 
+const METHODS: [&str; 4] = ["zip", "tar", "tar.gz", "tar.zst"];
+
+fn method_url_query(path: &String, method: &str) -> String {
+    format!("{path}?method={method}")
+}
+
 #[component]
 pub fn FolderDownloads(path: Signal<PathBuf>) -> impl IntoView {
+    let base_name = path.with(|path| display_os_string(path.file_name().unwrap_or_default()));
+    let base_path = Arc::new(format!(
+        "/archive/{}",
+        path.with(|path| display_os_string(path))
+    ));
+    let curl_list_path = Arc::clone(&base_path);
+
     let method_list = move || {
-        let path = path.with(|path| display_os_string(path));
-        ["zip", "tar", "tar.gz", "tar.zst"].map(|method| {
+        let base_path = &*base_path;
+        METHODS.map(|method| {
             view! {
               <li>
-                <a href=format!("/archive/{path}?method={method}") class="px-3 min-w-20" download>
+                <a href=method_url_query(base_path, method) class="px-3 min-w-20" download>
                   {method}
                 </a>
+              </li>
+            }
+        })
+    };
+
+    let fast_lan_list = move || {
+        let curl_list_path = &*curl_list_path;
+        METHODS.map(|method| {
+            let curl_url = method_url_query(curl_list_path, method);
+            view! {
+              <li>
+                <button
+                  class="px-3 min-w-20 text-left"
+                  title=format!("Copies: curl -L <this server>{curl_url} | tar --zstd -xvC ./{base_name}")
+                  data-url=curl_url.clone()
+                  onclick=format!(
+                    "navigator.clipboard.writeText(`curl -L \"${{window.location.origin}}{curl_url}\" | tar --zstd -xvC ./{base_name}`)",
+                  )
+                >
+                  Copy curl | {method}
+                </button>
               </li>
             }
         })
@@ -26,6 +60,8 @@ pub fn FolderDownloads(path: Signal<PathBuf>) -> impl IntoView {
         </label>
         <ul tabindex="0" class="p-2 shadow dropdown-content menu bg-base-100 rounded-box">
           {method_list}
+          <li class="menu-title">Fast LAN</li>
+          {fast_lan_list}
         </ul>
       </div>
     }
