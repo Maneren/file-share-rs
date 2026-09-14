98bd47dccdc349a60f1f5bcd028a39cffa72d4fc
Maneren <maneren731@gmail.com>
2026-09-14 02:32:45 +0200

    make tower compression smarter

--- server/src/fileserv.rs
+++ server/src/fileserv.rs
@@ -150,16 +150,13 @@ fn handle_archive(path: PathBuf, method: Option<&String>) -> impl IntoResponse +
         }
     });
 
-    let headers: [(_, HeaderValue); 6] = [
+    let headers: [(_, HeaderValue); 3] = [
         (
             header::CONTENT_DISPOSITION,
             format!(r#"attachment; filename="{file_name}""#).parse(),
         ),
         (header::CONTENT_TYPE, archive_method.mimetype().parse()),
-        (header::TRANSFER_ENCODING, "chunked".parse()),
         (header::CACHE_CONTROL, "no-cache".parse()),
-        (header::CONNECTION, "keep-alive".parse()),
-        (header::CONTENT_ENCODING, "identity".parse()),
     ]
     .map(|(key, value)| (key, value.expect("The headers are valid")));
 
