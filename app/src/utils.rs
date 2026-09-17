2fd4d8e04b3588d35e84e8f388d5c9a61030daed
Maneren <maneren731@gmail.com>
2026-09-17 22:25:22 +0200

    Encode URL paths per component so slashes survive

--- app/src/utils.rs
+++ app/src/utils.rs
@@ -58,8 +58,12 @@ pub fn display_os_string(str: impl AsRef<OsStr>) -> String {
     str.as_ref().to_string_lossy().into_owned()
 }
 
-pub fn encode_path(path: impl AsRef<OsStr>) -> String {
-    urlencoding::encode(path.as_ref().to_string_lossy().as_ref()).into_owned()
+pub fn encode_path(path: impl AsRef<Path>) -> String {
+    path.as_ref()
+        .components()
+        .map(|component| urlencoding::encode(&display_os_string(component)).into_owned())
+        .collect::<Vec<_>>()
+        .join("/")
 }
 
 #[must_use]
